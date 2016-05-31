f453d6c48345df830a8fd86491c1aca3ac123a03
hjyoon <se1620236@naver.com>
2016-05-31 21:40:42 +0900

    Update func.h

--- func.h
+++ func.h
@@ -8,8 +8,13 @@
 
 
 //** UI 함수들
-void select();
-
+void select();			//** 처음 선택문
+void select_1();		//** 로그인 후 선택문
+void order();			//** 주문
+void spend_point();		//** 마일리지 사용여부
+void charge_cash();		//** 캐쉬 충전
+void check_point();		//** 캐쉬 및 마일리지 확인
+void member_leave();	//** 회원탈퇴
 
 //** DB 함수들
 void login();
